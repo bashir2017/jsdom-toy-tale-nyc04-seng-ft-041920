3c79ca8609f00a53e6733c1997f9f349ecf4c556
bashir2017 <bashiralhanshali@yahoo.com>
2020-06-26 14:47:25 -0400

    some refactor

--- src/index.js
+++ src/index.js
@@ -16,12 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
   let toyCollection = document.querySelector("#toy-collection")
   let form = document.querySelector(".add-toy-form")
 
+  //initial loading of data 
   fetch('http://localhost:3000/toys')
   .then(resp => resp.json())
   .then(toys => {
     renderAllToys(toys)
   })
 
+  //handle form submission 
   form.addEventListener("submit", (event) => {
     event.preventDefault()
     const newToy = {
@@ -29,25 +31,22 @@ document.addEventListener("DOMContentLoaded", () => {
       image: form.image.value,
       likes: 0
     }
-    fetch('http://localhost:3000/toys',
-      {
-        method : 'POST',
-        body: JSON.stringify(newToy),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
+    const config =     {
+      method : 'POST',
+      body: JSON.stringify(newToy),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
       }
+    }
 
-    ).then(resp => resp.json())
-     .then(toy => {
-       console.log(toy)
-       renderToy(toy)
-     })
+    fetch('http://localhost:3000/toys',config)
+     .then(resp => resp.json())
+     .then(toy => renderToy(toy))
+     form.reset()
   })
 
-
-
+//render a single toy 
   function renderToy(toy){
     let toyContainer = document.createElement('div')
     toyContainer.classList = 'card'
@@ -61,30 +60,30 @@ document.addEventListener("DOMContentLoaded", () => {
     toyCollection.appendChild(toyContainer)
     let likeBtn =  toyContainer.querySelector('button')
 
-    likeBtn.addEventListener('click', (e) => {
-      let likesSpan = toyContainer.querySelector('span')
-      let likes = parseInt(likesSpan.innerText)
-      likesSpan.innerText = likes + 1
-      fetch(`http://localhost:3000/toys/${toy.id}`, 
-      {
-        method: "PATCH",
-        body: JSON.stringify({"likes" : likes + 1}),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json"
-        }
-      }
-      )
-      .then(resp => resp.json())
-      .then(toy => {
-         console.log(toy.likes)
-      })
-
-
+    likeBtn.addEventListener('click', () => {
+      updateLike(toy.id, toyContainer)
     })
   }
 
+//updated likes count for a toy 
+  function updateLike (id, toyContainer){
+    let likesSpan = toyContainer.querySelector('span')
+    let likes = parseInt(likesSpan.innerText)
+    likesSpan.innerText = likes + 1
+    const config = {
+      method: "PATCH",
+      body: JSON.stringify({"likes" : likes + 1}),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    }
+    fetch(`http://localhost:3000/toys/${id}`, config)
+    .then(resp => resp.json())
+    .then(toy => toy)
+  }
 
+//redner all toys 
   function renderAllToys(toyObjects){
     toyObjects.forEach((toy) => {
       renderToy(toy)
